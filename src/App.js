797a2bc06894824514791293670472c950f9e131
Alex Lee <alex@uniteus.com>
2018-12-06 21:57:42 -0500

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { UserSession } from 'blockstack'
+import { Container, Navbar } from 'react-bulma-components';
 import Login from 'pages/Login';
-import { Container } from 'react-bulma-components';
 import './App.scss';
 
 class App extends Component {
   state = {
     userSession: new UserSession(),
+    open: false,
   }
 
   componentDidMount = async () => {
@@ -21,17 +22,52 @@ class App extends Component {
     }
   }
 
+  toggleNavbar = () => {
+    this.setState({ open: !this.state.open })
+  }
+
   render() {
-    const { userSession } = this.state
+    const { userSession, open } = this.state
 
     return (
-      <Container className="App">
-        {
-          userSession.isUserSignedIn() ?
-            <div>Signed in page</div> :
-            <Login />
-        }
-      </Container>
+      <div className="App">
+        <Navbar
+          color="primary"
+          fixed="top"
+          active={open}
+        >
+        <Navbar.Brand>
+          <Navbar.Item renderAs="a" href="#">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              alt="Bulma: a modern CSS framework based on Flexbox"
+              width="112"
+              height="28"
+            />
+          </Navbar.Item>
+
+          <Navbar.Burger
+            onClick={this.toggleNavbar}
+          />
+        </Navbar.Brand>
+
+        <Navbar.Menu>
+          <Navbar.Container position="end">
+            <Navbar.Item href="/">Home</Navbar.Item>
+            <Navbar.Item href="/about"> About Us</Navbar.Item>
+            <Navbar.Item href="/contact"> Contact Us</Navbar.Item>
+          </Navbar.Container>
+        </Navbar.Menu>
+        </Navbar>
+
+        <Container>
+          {
+            userSession.isUserSignedIn() ?
+              <div>Signed in page</div> :
+              <Login />
+          }
+        </Container>
+      </div>
     );
   }
 }
